c4790d44ff87b214248930754bea2d7f2036bf59
Craig Edwards <craig@blackdogfoundry.com>
2018-10-22 07:14:53 +1100

    Make network API mockable and add test cases

--- lib/redux/holiday_list/holiday_list_middleware.dart
+++ lib/redux/holiday_list/holiday_list_middleware.dart
@@ -1,36 +1,27 @@
-import 'dart:async';
-import 'dart:math';
-
 import 'package:holidays/model/fetchable.dart';
-import 'package:holidays/model/holiday_summary.dart';
+import 'package:holidays/networking/api.dart';
 import 'package:holidays/redux/app/app_state.dart';
 import 'package:holidays/redux/holiday_list/holiday_list_actions.dart';
 import 'package:redux/redux.dart';
 
-List<Middleware<AppState>> createHolidayListMiddleware() => [
-      TypedMiddleware<AppState, FetchHolidaySummariesAction>(_fetchHolidaySummaries),
+List<Middleware<AppState>> createHolidayListMiddleware(API api) => [
+      TypedMiddleware<AppState, FetchHolidaySummariesAction>(_fetchHolidaySummaries(api)),
     ];
 
-Future _fetchHolidaySummaries(Store<AppState> store, FetchHolidaySummariesAction action, NextDispatcher next) async {
-  next(action);
+Middleware<AppState> _fetchHolidaySummaries(API api) {
+  return (Store<AppState> store, action, NextDispatcher next) async {
+    next(action);
 
-  if (action is FetchHolidaySummariesAction) {
-    // we'd normally call out to a repository of some kind, but for now,
-    // we'll just pause for a bit
-    await Future.delayed(const Duration(seconds: 1));
+    if (action is FetchHolidaySummariesAction) {
+      try {
+        final summaries = await api.fetchHolidaySummaries();
 
-    // let the UI know that we're back from the network
-    action.completer.complete();
-
-    // to let us build and test our retry logic, sometimes we'll return
-    // some holiday summaries and sometimes we'll return an error
-    if (Random().nextBool()) {
-      final summary1 = HolidaySummary(id: 1, name: 'Europe');
-      final summary2 = HolidaySummary(id: 2, name: 'America');
-
-      next(ReceivedHolidaySummariesAction(Fetchable.success([summary1, summary2])));
-    } else {
-      next(ReceivedHolidaySummariesAction(Fetchable.error(StateError("Network timed out"))));
+        action.completer.complete();
+        store.dispatch(ReceivedHolidaySummariesAction(Fetchable.success(summaries)));
+      } catch (e) {
+        action.completer.completeError(e);
+        store.dispatch(ReceivedHolidaySummariesAction(Fetchable.error(e)));
+      }
     }
-  }
+  };
 }
